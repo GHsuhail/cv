369b07d1d85ebd5cb2f35bbaa442be89abdbb54b
Suhail Bakr <suhailbakr@gmail.com>
2025-04-21 05:21:06 +0300

    the project is done

--- JS/main.js
+++ JS/main.js
@@ -15,3 +15,20 @@ mode.onclick= function()
         body.classList.remove("dark-mode");
     }
 };
+
+
+const goUpButton = document.querySelector(".go-up");
+
+window.onscroll = function () {
+if (window.scrollY > 500) 
+{
+    goUpButton.style.display = "block";
+} else
+    {
+    goUpButton.style.display = "none";
+    }
+};
+
+goUpButton.onclick = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
